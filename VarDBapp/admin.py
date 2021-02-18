bb0f2a2db21e2bb04ccb8f8488e94fb851af9ba6
NatalieC15 <lillie_a.c@hotmail.co.uk>
2021-02-18 15:07:10 +0000

    Added superusers and tables to admin

--- VarDBapp/admin.py
+++ VarDBapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Gene, Transcript, Genome, Variant_description, Evidence, Pathogenicity, Sequencer, Phenotype, Patient, Variant_instance
 
 # Register your models here.
+admin.site.register(Gene)
+admin.site.register(Transcript)
+admin.site.register(Genome)
+admin.site.register(Variant_description)
+admin.site.register(Evidence)
+admin.site.register(Pathogenicity)
+admin.site.register(Sequencer)
+admin.site.register(Phenotype)
+admin.site.register(Patient)
+admin.site.register(Variant_instance)
