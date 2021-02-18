0eb7dff19909969353b57bb39351b8d42442b8f2
Becky-Sadler <rebeccaannesadler@gmail.com>
2021-02-18 16:05:52 +0000

    Addition of webpages to views.py and urls.py

--- VarDBapp/urls.py
+++ VarDBapp/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
+    path('home_page', views.home_page),
+    path('file_upload', views.file_upload),
+    path('single_upload', views.single_upload),
+    path('browse_variants', views.browse_variants),
+    path('search_result', views.search_result),
+    path('variant_browser', views.variant_browser),
+    path('edit_variant', views.edit_variant),
+    path('edit_sample', views.edit_sample),
 ]
--- VarDBapp/views.py
+++ VarDBapp/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'VarDBapp/home_page.html', {})
+
+def file_upload(request):
+    return render(request, 'VarDBapp/file_upload.html', {})
+
+def single_upload(request):
+	return render(request, 'VarDBapp/single_upload', {})
+
+def browse_variants(request):
+	return render(request, 'VarDBapp/browse_variants', {})
+
+def search_result(request):
+	return render(request, 'VarDBapp/search_result', {})
+
+def variant_browser(request):
+	return render(request, 'VarDBapp/variant_browser', {})
+
+def edit_variant(request):
+	return render(request, 'VarDBapp/edit_variant', {})
+
+def edit_sample(request):
+	return render(request, 'VarDBapp/edit_sample', {})
