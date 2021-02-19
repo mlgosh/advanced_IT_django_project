b343ac8defcefe78db7444a7138a3cad95eb16dc
maria <maria.lock@gosh.nhs.uk>
2021-02-19 14:27:08 +0000

    WIP: Parse variant file as csv and upload data to DB

--- VarDBapp/csv_parser/csv_parser.py
+++ VarDBapp/csv_parser/csv_parser.py
@@ -1,16 +1,16 @@
 import copy
 import csv
 #from loguru import logger
-#from VarDBapp import models # need this?
+from VarDBapp import models
 import re
 import warnings
 
 variant_file = 'brca1_vars.csv'
 
-class CSVParser():
+class CSVParser:
 
-    def __init__(self):
-        self.variant_file = variant_file
+    def __init__(self, variant_file_instance):
+        self.variant_file = variant_file_instance
         self.sequencers = ['Miseq', 'Hiseq']
 
     def csv_reader(self):
@@ -37,7 +37,7 @@ class CSVParser():
         """
         if key == 'Sequencer':
             if dictionary[key] not in self.sequencers:
-                raise warnings
+                raise warnings.warn(f'{dictionary[key]} is not a valid sequencer. If this is incorrect please ask admin for the sequencer to be added to the database.')
                 # Want to add functionality to check if it's an error with the cases
         elif key == 'SampleID':
             episode_pattern = re.compile("20[0-9]{2}_[0-9]*")
@@ -58,15 +58,43 @@ class CSVParser():
                 print(code)
                 code_pattern = re.compile("[PB]V?[MSP][1-7]")
                 if not re.match(code_pattern, code):
-                    raise warnings
+                    raise warnings.warn(f'{code} is not a valid evidence code.')
         return dictionary
 
-    def add_variants(self):
+    def add_variants(self, dictionary):
         """
         This function adds the results to the ? table(s)
+
+        For now I've added the code from uploading gel data - let's try and get this working
         """
-        
-        pass
+
+        # Need to populate each table in order I think
+
+        gene, created = models.Gene.objects.update_or_create(
+            gene_name=dictionary['Gene name'],
+            chrom=dictionary['Chromosome'] # We need to query an API for this
+        )
+
+        variant_description, created = models.variant_description.objects.update_or_create(
+            transcript=dictionary['Transcript'],
+            variant_cDNA=dictionary['Variant cDNA'],
+            variant_protein=dictionary['Variant Protein'],
+            variant_genome=dictionary['Variant Genome'],
+            genome_build=dictionary['Genome Assembly']
+        )
+
+        phenotype, created = models.Phenotype.object.update_or_create(
+            description=dictionary['Description']
+            stage=dictionary['Stage']
+        )
+
+        patient, created = models.Patient.objects.update_or_create(
+            patient_id=dictionary['SampleID'],
+            age=dictionary['Age'],
+            proband=dictionary['Proband'],
+            affected_relatives=dictionary['Affected Relatives']
+
+        )
 
     def main(self):
         csv_dicts = self.csv_reader()
@@ -75,9 +103,9 @@ class CSVParser():
                 dictionary[key] = dictionary[key].strip()
                 dictionary = self.process_bools(dictionary, key)
                 dictionary = self.split_evidence(dictionary, key)
-            break
+            self.add_variants(dictionary)
 
 
-if __name__ == "__main__":
-    csv_parser_runner = CSVParser()
-    csv_parser_runner.main()
+#if __name__ == "__main__":
+ #   csv_parser_runner = CSVParser()
+  #  csv_parser_runner.main()
