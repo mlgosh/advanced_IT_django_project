8615626b099e9b561d21e48b554dcc8f2721f27d
NatalieC15 <lillie_a.c@hotmail.co.uk>
2021-02-19 08:39:39 +0000

    Added base.html for generate format, added authentication login html, and other htmls for the application

--- VarDBapp/views.py
+++ VarDBapp/views.py
@@ -1,28 +1,31 @@
 from django.shortcuts import render
 from django.utils import timezone
-from .model import *
+from .models import *
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home_page(request):
     return render(request, 'VarDBapp/home_page.html', {})
 
+@login_required
 def file_upload(request):
     return render(request, 'VarDBapp/file_upload.html', {})
 
+@login_required
 def single_upload(request):
-	return render(request, 'VarDBapp/single_upload.html', {})
+    return render(request, 'VarDBapp/single_upload.html', {})
 
 def browse_variants(request):
-	return render(request, 'VarDBapp/browse_variants.html', {})
+    return render(request, 'VarDBapp/browse_variants.html', {})
 
 def search_result(request, search_term):
-	return render(request, 'VarDBapp/search_result.html', {})
+    return render(request, 'VarDBapp/search_result.html', {})
 
 def variant_browser(request):
-	return render(request, 'VarDBapp/variant_browser.html', {})
+    return render(request, 'VarDBapp/variant_browser.html', {})
 
 def edit_variant(request):
-	return render(request, 'VarDBapp/edit_variant.html', {})
+    return render(request, 'VarDBapp/edit_variant.html', {})
 
 def edit_sample(request):
-	return render(request, 'VarDBapp/edit_sample.html', {})
+    return render(request, 'VarDBapp/edit_sample.html', {})
